6e67edd340dd53e19e2172d6726a7533e4f9bcea
nikollesantos <nikolledossantos@outlook.com>
2025-01-17 16:03:50 -0300

    styling Projects Page

--- penta-portfolio/src/Components/FeatureProjects/FeatureProjects.js
+++ penta-portfolio/src/Components/FeatureProjects/FeatureProjects.js
@@ -4,71 +4,52 @@ import './FeatureProjects.scss';
 
 const FeatureProjects = () => {
     return (
-        <div className="container feat-proj">
-            {/* Título da seção */}
-            <div className="section-title">
-                <h3>Feature Projects</h3>
+        <div className="projects-page">
+            <div className='title-projects'>
+                <h3 className='projects-page-title'>PROJECTS</h3>
             </div>
-
-            {/* Primeira linha */}
-            <div className="row">
-                {/* Coluna 1 */}
-                <div className="col s4 m4 l4 project-title">
-                    <h1>projects</h1>
+            <div className='projects-preview'>
+                <div className='container'>
+                    <div className='row'>
+                        <h4 
+                            className='col s4 m4 l4 project-title' 
+                            style={{textAlign:"right"}}>
+                                doctor luciano gazzoni
+                        </h4>
+                        <p className='col s4 m4 l4 project-text'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum 
+                            euismod ipsum ut pellentesque. Sed porta leo neque, sit amet placerat tortor 
+                            venenatis bibendum. Fusce mattis scelerisque aliquam. Curabitur feugiat mi quis 
+                            dolor volutpat, sit amet viverra neque maximus. Pellentesque quis tellus ullamcorper 
+                            nunc dapibus fringilla in tincidunt dolor.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum 
+                            euismod ipsum ut pellentesque. Sed porta leo neque, sit amet placerat tortor 
+                            venenatis bibendum. Fusce mattis scelerisque aliquam. Curabitur feugiat mi quis 
+                            dolor volutpat, sit amet viverra neque maximus. Pellentesque quis tellus ullamcorper 
+                            nunc dapibus fringilla in tincidunt dolor.
+                        </p>
+                        <img className='col s4 m4 l4 project-img' src='/images/projects/projectsPageImg1.png'></img>
+                    </div>
                 </div>
-
-                {/* Coluna 2 - Texto */}
-                <div className="col s4 m4 l4">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
-                        fermentum euismod ipsum ut pellentesque. Sed porta leo neque, sit 
-                        amet placerat tortor venenatis bibendum. Fusce mattis scelerisque 
-                        aliquam. Curabitur feugiat mi quis dolor volutpat, sit amet viverra 
-                        neque maximus. Pellentesque quis tellus ullamcorper nunc dapibus fringilla 
-                        in tincidunt dolor. Integer ultricies mollis nunc sit amet hendrerit. 
-                        Aliquam porttitor velit felis, quis ornare purus eleifend vitae. Phasellus 
-                        tortor orci, vulputate nec justo sit amet, feugiat bibendum urna. Praesent 
-                        nisl nibh, rhoncus et eros vitae, interdum porta quam.
-                    </p>
+                <div className='container'>
+                    <div className='row'>
+                        <p className='col s4 m4 l4 project-text'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum 
+                                euismod ipsum ut pellentesque. Sed porta leo neque, sit amet placerat tortor 
+                                venenatis bibendum. Fusce mattis scelerisque aliquam. Curabitur feugiat mi quis 
+                                dolor volutpat, sit amet viverra neque maximus. Pellentesque quis tellus ullamcorper 
+                                nunc dapibus fringilla in tincidunt dolor.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum 
+                                euismod ipsum ut pellentesque. Sed porta leo neque, sit amet placerat tortor 
+                                venenatis bibendum. Fusce mattis scelerisque aliquam. Curabitur feugiat mi quis 
+                                dolor volutpat, sit amet viverra neque maximus. Pellentesque quis tellus ullamcorper 
+                                nunc dapibus fringilla in tincidunt dolor.
+                        </p>
+                        <img className='col s4 m4 l4 project-img' src='/images/projects/projectsPageImg2.png'></img>
+                        <h4 className='col s4 m4 l4 project-title'>fora da caixa</h4>
+                    </div>
                 </div>
-
-                {/* Coluna 3 - Imagem */}
-                <div className="col s4 m4 l4 img">
-                    <img src="/images/carousel/imgOne.jpg" alt="Project Example" />
-                </div>
-            </div>
-
-            {/* Segunda linha */}
-            <div className="row">
-                {/* Coluna 1 - Texto */}
-                <div className="col s4 m4 l4">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
-                        fermentum euismod ipsum ut pellentesque. Sed porta leo neque, sit 
-                        amet placerat tortor venenatis bibendum. Fusce mattis scelerisque 
-                        aliquam. Curabitur feugiat mi quis dolor volutpat, sit amet viverra 
-                        neque maximus. Pellentesque quis tellus ullamcorper nunc dapibus fringilla 
-                        in tincidunt dolor. Integer ultricies mollis nunc sit amet hendrerit. 
-                        Aliquam porttitor velit felis, quis ornare purus eleifend vitae. Phasellus 
-                        tortor orci, vulputate nec justo sit amet, feugiat bibendum urna. Praesent 
-                        nisl nibh, rhoncus et eros vitae, interdum porta quam.
-                    </p>
-                </div>
-
-                {/* Coluna 2 - Imagem */}
-                <div className="col s4 m4 l4 img">
-                    <img src="/images/carousel/imgOne.jpg" alt="Project Example 2" />
-                </div>
-
-                {/* Coluna 3 - Título */}
-                <div className="col s4 m4 l4 project-title">
-                <h1>projects</h1>
-                </div>
-            </div>
-
-            {/* Link de navegação */}
-            <div className="view-all">
-                <a href="#!">View all projects</a>
+                <div></div>
             </div>
         </div>
     );
