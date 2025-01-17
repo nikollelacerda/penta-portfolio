a0748b9215e37aa5bbe663911a620633489a9833
nikollesantos <nikolledossantos@outlook.com>
2025-01-17 13:37:05 -0300

    fixing image and links position

--- penta-portfolio/src/Components/Navbar/Navbar.js
+++ penta-portfolio/src/Components/Navbar/Navbar.js
@@ -1,38 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import "./Navbar.scss";
 
 const Navbar = () => {
+    
     useEffect(() => {
         const elems = document.querySelectorAll(".sidenav");
         M.Sidenav.init(elems);
     }, []);
 
     return (
-        <nav className="navbar">
-            <div className="container">
-                <div className="nav-wrapper row valign-wrapper">
-                    {/* Logo */}
-                    <div className="col s4 m4 l4">
-                        <a href="#" className="brand-logo">
-                            <img 
-                                src="/images/logo/logoPenta.png" 
-                                alt="Logo" 
-                                className="logo-img responsive-img" 
-                            />
-                        </a>
-                    </div>
-
-                    {/* Navbar Links */}
-                    <ul id="nav-mobile" className="right hide-on-med-and-down col s8 m8 l8">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">JavaScript</a></li>
-                    </ul>
-                </div>
+        <nav>
+          <div>
+            <div class="nav-wrapper navbar">
+              <a href="#" class="brand-logo">
+                <img src="/images/logo/logoPenta.png" className="logo-img"></img>
+              </a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">HOME</a></li>
+                <li><a href="badges.html">ABOUT</a></li>
+                <li><a href="collapsible.html">CONTACT</a></li>
+              </ul>
             </div>
-        </nav>
+        </div>
+      </nav>
     );
 };
 
